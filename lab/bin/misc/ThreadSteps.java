d365c87b4945daae27f9be69975d240f2e4960a5
Cole Nielson <109990741+Jhkdk@users.noreply.github.com>
2023-12-09 17:45:46 -0700

    Update ThreadSteps.java

--- lab/bin/misc/ThreadSteps.java
+++ lab/bin/misc/ThreadSteps.java
@@ -3,34 +3,38 @@ package misc;
 import java.util.Map;
 
 public class ThreadSteps extends Thread {
-	private int steps;
-	private Map<String,String> map;
+	private long steps;
+	private Map<String, String> map;
 	private String directions;
 	private String current;
-	public ThreadSteps(Map<String,String> map, String directions, String current) {
+
+	public String getCurrent() {
+		return current;
+	}
+
+	public ThreadSteps(Map<String, String> map, String directions, String current) {
 		this.map = map;
 		this.directions = directions;
 		this.current = current;
 	}
-	
+
 	public void main() {
-		steps = step(directions, map, current);
+		step(directions, map);
 	}
-	
-	public int getSteps() {
+
+	public void run() {
+		step(directions, map);
+	}
+
+	public long getSteps() {
 		return steps;
 	}
 
-	public void setSteps(int steps) {
-		this.steps = steps;
-	}
-
-	public static int step(String directions, Map<String, String> map, String current) {
-		int steps = 0;
-		while (!current.contains("ZZZ")) {
-			current = map.get(current).split(" ")[directions.charAt(steps % directions.length()) == 'R' ? 1 : 0];
+	public void step(String directions, Map<String, String> map) {
+		while (!current.endsWith("Z")) {
+			current = map.get(current).split(" ")[directions.charAt((int) (steps % directions.length())) == 'R' ? 1
+					: 0];
 			steps++;
 		}
-		return steps;
 	}
 }
